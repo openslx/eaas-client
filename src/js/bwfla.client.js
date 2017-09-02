3315e3e3a7ba14f10d07ed7c8ba321bf8ff0d634
klaus <klaus@klauss-MacBook-Pro.local>
2017-09-02 09:25:56 +0200

    print api

--- src/js/bwfla.client.js
+++ src/js/bwfla.client.js
@@ -262,6 +262,10 @@ EaasClient.Client = function (api_entrypoint, container) {
         return API_URL + formatStr("/components/{0}/screenshot", _this.componentId);
     };
 
+    this.getPrintUrl = function() {
+        return API_URL + formatStr("/components/{0}/print", _this.componentId);
+    };
+
     this.stopEnvironment = function () {
         this.guac.disconnect();
         $(container).empty();
