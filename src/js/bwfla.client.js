a57c62ca96dd2d9002f130d7b41421dad2351bbc
Oleg Zharkov <jos.to.new@gmail.com>
2017-09-07 11:38:13 +0200

    fix xpra-guacamole if-clause

--- src/js/bwfla.client.js
+++ src/js/bwfla.client.js
@@ -61,7 +61,7 @@ EaasClient.Client = function (api_entrypoint, container) {
                              * XPRA Section
                              */
 
-                            if (data.toString().indexOf("xpra") > -1) {
+                            if (typeof data.xpra !== "undefined" > -1) {
                                 console.log("my link" + data.xpra);
                                 _this.keepaliveIntervalId = setInterval(_this.keepalive, 1000);
                                 /**
