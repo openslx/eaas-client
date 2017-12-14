71a73450d13a430ac4a8a7e2062550a5c08d6b4e
Rafael Gieschke <rafael.gieschke@rz.uni-freiburg.de>
2017-12-14 11:24:00 +0100

    Add script to sync third_party/vendor from upstream

---
+++ resources/webemulator/v86/sync-third-party.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+set -eu
+
+REPO="https://github.com/copy/v86/"
+NAME="v86"
+
+build() {
+  make build/libv86.js build/v86_all.js
+  rm -rf .git/
+  rm -rf "$DEST/third_party/""$NAME"
+  cp -R . "$DEST/third_party/""$NAME"
+}
+
+add() {
+  git add third_party/"$NAME"
+}
+
+. ../../../sync-git.lib.sh
