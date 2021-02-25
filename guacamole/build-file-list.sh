471076049f64993cf0f69545b7041c87bd858880
Rafael Gieschke <rafael@gieschke.de>
2021-02-25 17:52:08 +0100

    guacamole: Add symlinks in dist/ to directories to be distributed

--- guacamole/build-file-list.sh
+++ guacamole/build-file-list.sh
@@ -5,12 +5,12 @@ cd -- "$(dirname -- "$0")"
 (
 echo "export const jsFiles = ["
 printf '"%s",\n' \
-    ./guacamole-client-eaas/guacamole-common-js/src/main/webapp/modules/*.js
+    ./dist/modules/*.js
 echo "];"
 echo
 echo "export const cssFiles = ["
 printf '"%s",\n' \
-  guacamole-client-eaas/guacamole/src/main/webapp/app/client/styles/keyboard.css \
-  guacamole-client-eaas/guacamole/src/main/webapp/app/osk/styles/osk.css
+  ./dist/styles/keyboard.css \
+  ./dist/osk/styles/osk.css
 echo "];"
 ) > file-list.js
