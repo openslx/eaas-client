9bbba3fc3ea8e44331dd5dd9f48bee4e2944c649
Rafael Gieschke <rafael@gieschke.de>
2021-02-25 12:10:28 +0100

    guacamole: Simplify build process
    This drops `layouts/en-us-qwerty*.xml`.

--- guacamole/build-third-party.sh
+++ guacamole/build-third-party.sh
@@ -2,15 +2,23 @@
 
 cd -- "$(dirname -- "$0")"
 
+rm guacamole.js guacamole.css || :
+
+help() {
+  echo You might want to run:
+  echo
+  echo git submodule update -f guacamole-client guacamole-client-css
+  exit 1
+}
+
 (
 cd guacamole-client
-git apply ../0001-Patches-from-eaas-client.patch
+git apply ../0001-Patches-from-eaas-client.patch || help
+cat guacamole-common-js/src/main/webapp/modules/*.js > ../guacamole.js
 )
 
 (
 cd guacamole-client-css
-git apply ../0001-keyboard.css-Patches-from-eaas-client.patch
+git apply ../0001-keyboard.css-Patches-from-eaas-client.patch || help
+cp guacamole/src/main/webapp/styles/keyboard.css ../guacamole.css
 )
-
-cat third_party/js/*.js > guacamole.js
-cat third_party/css/*.css > guacamole.css
