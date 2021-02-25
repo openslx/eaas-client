2e86bce589732e30451f069842761f831d6cf43e
Rafael Gieschke <rafael@gieschke.de>
2021-02-25 16:07:16 +0100

    Move Guacamole client patches to own Git repository

--- guacamole/build-third-party.sh
+++ guacamole/build-third-party.sh
@@ -2,23 +2,8 @@
 
 cd -- "$(dirname -- "$0")"
 
-rm guacamole.js guacamole.css || :
-
-help() {
-  echo You might want to run:
-  echo
-  echo git submodule update -f guacamole-client guacamole-client-css
-  exit 1
-}
-
-(
-cd guacamole-client
-git apply ../0001-Patches-from-eaas-client.patch || help
-cat guacamole-common-js/src/main/webapp/modules/*.js > ../guacamole.js
-)
-
-(
-cd guacamole-client-css
-git apply ../0001-keyboard.css-Patches-from-eaas-client.patch || help
-cp guacamole/src/main/webapp/styles/keyboard.css ../guacamole.css
-)
+cat guacamole-client-eaas/guacamole-common-js/src/main/webapp/modules/*.js > guacamole.js
+cat \
+  guacamole-client-eaas/guacamole/src/main/webapp/app/client/styles/keyboard.css \
+  guacamole-client-eaas/guacamole/src/main/webapp/app/osk/styles/osk.css \
+  > guacamole.css
