15b935e7ab3a32abbe593a66c5f7a54d3b16bb64
Rafael Gieschke <rafael@gieschke.de>
2021-02-25 11:57:40 +0100

    Use Guacamole client from upstream Git repository

--- guacamole/build-third-party.sh
+++ guacamole/build-third-party.sh
@@ -1,5 +1,16 @@
-#!/bin/sh
+#!/bin/sh -xeu
 
 cd -- "$(dirname -- "$0")"
+
+(
+cd guacamole-client
+git apply ../0001-Patches-from-eaas-client.patch
+)
+
+(
+cd guacamole-client-css
+git apply ../0001-keyboard.css-Patches-from-eaas-client.patch
+)
+
 cat third_party/js/*.js > guacamole.js
 cat third_party/css/*.css > guacamole.css
