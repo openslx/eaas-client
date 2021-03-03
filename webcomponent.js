6c38428cd38a354e10aa243a993e2109e09a8b2d
Rafael Gieschke <rafael@gieschke.de>
2021-03-03 23:56:50 +0100

    WIP: Add Web Component

---
+++ webcomponent.js
@@ -0,0 +1,118 @@
+import { Client } from "./eaas-client.js";
+import {
+  InputBuilder,
+  InputContentBuilder,
+  MachineComponentBuilder,
+} from "./lib/componentBuilder.js";
+import { ClientOptions } from "./lib/clientOptions.js";
+import { NetworkBuilder } from "./lib/networkBuilder.js";
+import { _fetch } from "./lib/util.js";
+
+const kbLayoutPrefs = {
+  language: { name: "us", description: "English (US)" },
+  layout: {
+    name: "pc101",
+    description: "Generic 101-key PC",
+    vendor: "Generic",
+  },
+};
+
+class EaasClientElement extends HTMLElement {
+  constructor() {
+    super();
+    const container = document.createElement("div");
+    container.id = "emulator-container";
+    this.append(container);
+    this.container = container;
+    this.backendUrl = this.getAttribute("eaas-service");
+    const { backendUrl } = this;
+    this.client = new Client(backendUrl, undefined, { kbLayoutPrefs });
+    const { client } = this;
+    this.style.display = "block";
+    this.style.width = "min-content";
+
+    const envId = this.getAttribute("environment-id");
+    const imageArchive = this.getAttribute("image-archive") || "public";
+    const networkId = this.getAttribute("network-id");
+    const networkLabel = this.getAttribute("network-label");
+    const containerId = this.getAttribute("src")?.match(/\/?([^/]+)$/)[1];
+    const networkName =
+      this.getAttribute("network-name") ?? `network-${Math.random()}`;
+    const machine = new MachineComponentBuilder(envId, imageArchive);
+    if (containerId)
+      machine.setLinuxRuntime({
+        userContainerEnvironment: containerId,
+        userContainerArchive: imageArchive,
+      });
+
+    let inputMedia;
+    const inputs = [...this.querySelectorAll("eaas-input")];
+    if (inputs.length) {
+      inputMedia = new InputBuilder("/input");
+      for (const input of inputs) {
+        const contentBuilder = new InputContentBuilder(
+          input.getAttribute("href")
+        );
+        contentBuilder.setName(input.getAttribute("name"));
+        const format = input.getAttribute("format");
+        if (format) {
+          contentBuilder.setAction("extract");
+          contentBuilder.setCompressionFmt(format);
+        } else {
+          contentBuilder.setAction("copy");
+        }
+        inputMedia.addContent(contentBuilder);
+      }
+    }
+    if (inputMedia) machine.addInputMedia(inputMedia);
+
+    const monitorStateChanges = async () => {
+      let oldState;
+      while (true) {
+        const state = (
+          await this.session.getEmulatorState()
+        ).state.toLowerCase();
+        if (state !== oldState) {
+          this.dispatchEvent(new CustomEvent(state));
+        }
+        oldState = state;
+        console.log({ state });
+        await new Promise((r) => setTimeout(r, 1000));
+      }
+    };
+
+    const clientOptions = new ClientOptions();
+    (async () => {
+      if (networkId) {
+        const netUtils = new NetworkBuilder(backendUrl);
+        const session = await netUtils.connectNetworkSession(
+          client,
+          networkId,
+          networkName
+        );
+
+        const realSession = await _fetch(
+          `${client.API_URL}/sessions/${session.id}`,
+          "GET"
+        );
+        client.load(realSession);
+
+        const { componentId } = client.network.networkConfig.components.find(
+          (e) => e.networkLabel === networkLabel
+        );
+
+        const componentSession = client.getSession(componentId);
+        this.session = componentSession;
+        monitorStateChanges();
+      } else {
+        await client.start([machine], clientOptions);
+      }
+      await client.connect(container);
+      for (const el of [...this.childNodes])
+        if (el !== this.container) el.remove();
+    })();
+  }
+  attributeChangedCallback() {}
+}
+
+customElements.define("eaas-environment", EaasClientElement);
